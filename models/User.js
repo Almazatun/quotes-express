c14741bd6861f3e28b09bf2457eee7641baf78b9
Almaz <atunstyle@gmail.com>
2021-01-30 11:33:27 +0300

    update User Schema type

--- models/User.js
+++ models/User.js
@@ -2,10 +2,9 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const UserSchema = new Schema({
-    _id: mongoose.Schema.Types.ObjectId,
-    userName: {type: String, required: true},
-    email: {type: String, required: true},
-    password: {type: String, required: true},
+    userName: String,
+    email: String,
+    password: String,
     createdAt: String
 })
 
