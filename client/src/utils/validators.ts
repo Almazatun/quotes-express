3f69f352715222d18d25b7d8948dd024f4c0c6d1
Almaz <atunstyle@gmail.com>
2021-02-04 18:54:13 +0300

    add new post validators feature

--- client/src/utils/validators.ts
+++ client/src/utils/validators.ts
@@ -14,3 +14,22 @@ export const validators = (userName: string, password: string): {errors: Array<s
         valid: Object.keys(errors).length < 1
     }
 }
+
+export const newPostValidators = (postTitle: string, postBody: string): {errors: Array<string>, valid: boolean} => {
+    let errors: Array<string> = []
+
+    if (!postTitle) {
+        errors.push('New post title should be required')
+    } else if (postTitle && postTitle.trim().length > 20) {
+        errors.push('New post title not should be more than 20 characters')
+    }
+
+    if (!postBody) {
+        errors.push('New post content should be required')
+    }
+
+    return {
+        errors,
+        valid: Object.keys(errors).length < 1
+    }
+}
