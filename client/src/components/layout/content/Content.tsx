f99a8704c3d3de10d0564966224fb391b0d6a055
Almaz <atunstyle@gmail.com>
2021-02-05 18:54:20 +0300

    a little bit modify style and add new Router *Implemented Route of the PostBlog component.

--- client/src/components/layout/content/Content.tsx
+++ client/src/components/layout/content/Content.tsx
@@ -3,16 +3,21 @@ import style from './Content.module.scss'
 import {Route, Switch} from "react-router-dom";
 import {PAGES} from "../../Navbar/Navbar";
 import LogInContainer from "../../containers/LogInContainer/LogInContainer";
-import PostsContainer from "../../containers/PostsContainer/PostsContainer";
-import {WithAuthRedirect} from "../../withAuthRedirect/withAuthRedirect";
+import WrappedPostsContainer from "../../containers/PostsContainer/PostsContainer";
+import WrappedWritePostContainer from "../../containers/WritePostContainer/WritePostContainer";
+import WrappedPostBlogContainer from "../../containers/PostBlogContainer/PostBlogContainer";
+
+
 
 export class Content extends React.Component {
     render() {
         return (
             <div className={style.content}>
                 <Switch>
-                    <Route exact path={PAGES.POSTS} component={() =><WithAuthRedirect> <PostsContainer/> </WithAuthRedirect> }/>
+                    <Route exact path={PAGES.POSTS} component={() => <WrappedPostsContainer/>}/>
                     <Route exact path={PAGES.LOG_IN} component={() => <LogInContainer/>}/>
+                    <Route exact path={PAGES.WRITE_NEW_POST} component={() => <WrappedWritePostContainer/>}/>
+                    <Route exact path={`${PAGES.POST}/:postId?`} component={() => <WrappedPostBlogContainer/>}/>
                 </Switch>
             </div>
         )
