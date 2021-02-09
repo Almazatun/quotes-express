0cd10e9c56800d76f84c2f7e03d8e68cb2b55bee
Almaz <atunstyle@gmail.com>
2021-02-09 18:39:30 +0300

    update import component *ContentContainer component

--- client/src/appClient/App.tsx
+++ client/src/appClient/App.tsx
@@ -1,14 +1,14 @@
 import React, {Component} from 'react';
 import './App.scss';
 import {Header} from '../components/layout/header/Header';
-import { Content } from '../components/layout/content/Content';
+import ContentContainer from "../components/containers/ContentContainer/ContentContainer";
 
 class App extends Component {
     render() {
         return (
             <div className="App">
                 <Header/>
-                <Content/>
+                <ContentContainer/>
             </div>
         );
     }
