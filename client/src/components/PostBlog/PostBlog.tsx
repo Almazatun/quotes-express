219282c9a707b76f996a1c40e1f1e9250dced743
Almaz <atunstyle@gmail.com>
2021-02-09 19:06:32 +0300

    update, add new features and improve style

--- client/src/components/PostBlog/PostBlog.tsx
+++ client/src/components/PostBlog/PostBlog.tsx
@@ -6,7 +6,9 @@ import {NavLink} from 'react-router-dom';
 
 interface PostProps {
     postTitle?: string
-    post: IPost | undefined | null
+    post: IPost | undefined | null,
+    deletePost: () => void
+    showModal: () => void
 }
 
 export class PostBlog extends Component<PostProps, {}> {
@@ -16,21 +18,33 @@ export class PostBlog extends Component<PostProps, {}> {
 
     render() {
         return (
-            <div className={style.post_blog_container}>
-                <div className={style.prev_location}>
-                    <NavLink to={PAGES.POSTS}>
-                        <button>
-                            {" ⏪ Previous"}
-                        </button>
-                    </NavLink>
-                </div>
-                <div className={style.post_blog_content}>
-                    <h3>Post Title</h3>
-                    <div className={style.post_body}>
-                        <p>{this.props.post?.body}</p>
+                <div className={style.post_blog_container}>
+                    <div className={style.prev_location}>
+                        <NavLink to={PAGES.POSTS}>
+                            <button>
+                                Previous
+                            </button>
+                        </NavLink>
+                    </div>
+                    <div className={style.post_blog_content}>
+                        <div className={style.post_blog_update_btns}>
+                            <button className={style.edit_btn}
+                                    onClick={this.props.showModal}
+                            >
+                                Edit post
+                            </button>
+                            <button className={style.delete_btn}
+                                    onClick={this.props.deletePost}
+                            >
+                                Delete post
+                            </button>
+                        </div>
+                        <h3>{this.props.post?.postTitle}</h3>
+                        <div className={style.post_body}>
+                            <p>{this.props.post?.body}</p>
+                        </div>
                     </div>
                 </div>
-            </div>
         );
     }
 }
--- client/src/components/containers/PostBlogContainer/PostBlogContainer.tsx
+++ client/src/components/containers/PostBlogContainer/PostBlogContainer.tsx
@@ -5,12 +5,25 @@ import {selectPosts} from "../PostsContainer/Selectors";
 import {postsActions} from "../../Posts";
 import {connect, ConnectedProps} from "react-redux";
 import {WithAuthRedirect} from "../../../hoc/WithAuthRedirect/WithAuthRedirect";
-import { PostBlog } from "../../PostBlog/PostBlog";
-import { withRouter } from "react-router-dom";
+import {PostBlog} from "../../PostBlog/PostBlog";
+import {Redirect, withRouter} from "react-router-dom";
 import {RouteComponentProps} from "react-router";
+import {PAGES} from "../../Navbar/Navbar";
+import {ModalCard} from "../../shared/ModalCard/ModalCard";
+import {EditForm} from "../../shared/EditForm/EditForm";
+import {ChangeEventType} from "../WritePostContainer/WritePostContainer";
+import {newPostValidators} from "../../../utils/validators";
+import {NewPostBody} from "../../../api/api";
 
 interface PostBlogContainerProps {
     post: IPost | undefined | null
+    redirect: boolean,
+    editPost: {
+        postTitle: string
+        postBody: string
+    },
+    errors: Array<string>
+    showEditPostModal: boolean
 }
 
 export class PostBlogContainer extends Component<MergedT, PostBlogContainerProps> {
@@ -19,10 +32,21 @@ export class PostBlogContainer extends Component<MergedT, PostBlogContainerProps
         super(props);
 
         this.state = {
-            post: null
+            post: null,
+            redirect: false,
+            editPost: {
+                postTitle: '',
+                postBody: ''
+            },
+            errors: [],
+            showEditPostModal: false
         }
 
         this.findPost = this.findPost.bind(this)
+        this.deletePost = this.deletePost.bind(this)
+        this.onChangePostEditForm = this.onChangePostEditForm.bind(this)
+        this.modalShowPostModalHandler = this.modalShowPostModalHandler.bind(this)
+        this.editPost = this.editPost.bind(this)
     }
 
     componentDidMount() {
@@ -35,8 +59,8 @@ export class PostBlogContainer extends Component<MergedT, PostBlogContainerProps
         this.findPost(postId)
     }
 
-    componentDidUpdate(prevProps: Readonly<MergedT>, prevState: Readonly<PostBlogContainerProps>, snapshot?: any) {
-        if (this.props.posts !== prevProps.posts) {
+    componentDidUpdate(prevProps: Readonly<MergedT>) {
+        if (prevProps.posts !== this.props.posts) {
             const postId = this.props.match.params.postId
             this.findPost(postId)
         }
@@ -48,16 +72,109 @@ export class PostBlogContainer extends Component<MergedT, PostBlogContainerProps
             return postId === post._id
         })
         this.setState({
-            ...this.state,
             post: findPost
         })
+    }
 
+    deletePost() {
+        // eslint-disable-next-line no-restricted-globals
+        let sure = confirm("Do you really want to delete the post?")
+        if (sure) {
+            this.props.deletePostTC({postId: this.state.post!._id})
+            this.setState({
+                redirect: true
+            })
+        }
+    }
+
+    onChangePostEditForm (event: ChangeEventType) {
+        event.persist();
+        let value = event.currentTarget.value.trim() !== '' ? event.currentTarget.value : '';
+        if (event.currentTarget.dataset.field) {
+            const trigger: string = event.currentTarget.dataset.field;
+            if (trigger === "postTitle") {
+                if (value.length <= 20) {
+                    this.setState({
+                        editPost: {
+                            ...this.state.editPost,
+                            postTitle: value
+                        }
+                    })
+                }
+            } else if (trigger === "postBody") {
+                this.setState({
+                    editPost: {
+                        ...this.state.editPost,
+                        postBody: value
+                    }
+                })
+            }
+        }
+    }
+
+    editPost() {
+        const {valid, errors} = newPostValidators(this.state.editPost.postTitle, this.state.editPost.postBody)
+
+        if (errors) {
+            this.setState({
+                errors: [...errors]
+            })
+        }
+        if (this.state.errors) {
+            setTimeout(() => {
+                this.setState({
+                    errors: []
+                })
+            }, 1000)
+        }
+        if (valid) {
+            const {postTitle, postBody} = this.state.editPost
+            const updatePostBody: NewPostBody = {postTitle, body: postBody}
+            this.props.updatePostTC({postId: this.state.post!._id, updatePostBody})
+            this.setState({
+                editPost: {
+                    ...this.state.editPost,
+                    postTitle: '',
+                    postBody: '',
+                },
+            })
+            this.setState({
+                showEditPostModal: false
+            })
+        }
+    }
+
+    modalShowPostModalHandler () {
+        this.setState({
+            ...this.state,
+            showEditPostModal: !this.state.showEditPostModal,
+            editPost: {
+                postTitle: this.state.post!.postTitle,
+                postBody: this.state.post!.body
+            }
+        })
     }
 
     render() {
+        if (this.state.redirect) {
+            return <Redirect to={PAGES.POSTS}/>
+        }
         return (
             <Fragment>
-                <PostBlog post={this.state.post}/>
+                <PostBlog post={this.state.post}
+                          deletePost={this.deletePost}
+                          showModal={this.modalShowPostModalHandler}
+                />
+                <ModalCard show={this.state.showEditPostModal}
+                           closeModal={this.modalShowPostModalHandler}
+                >
+                    <EditForm title={this.state.editPost.postTitle}
+                              postBody={this.state.editPost.postBody}
+                              cancelHandler={this.modalShowPostModalHandler}
+                              onChangeHandler={this.onChangePostEditForm}
+                              submit={this.editPost}
+                    />
+                </ModalCard>
             </Fragment>
         )
     }
@@ -66,7 +183,8 @@ export class PostBlogContainer extends Component<MergedT, PostBlogContainerProps
 //Type
 type PostPick = Pick<PostsState, "posts">;
 
-export interface IPost extends Post {}
+export interface IPost extends Post {
+}
 
 interface Route {
     postId: string
@@ -79,9 +197,9 @@ const mapStateToProps = (state: AppRootStateType): PostPick => {
     }
 }
 
-const {deletePostTC, fetchPostsTC} = postsActions
+const {deletePostTC, fetchPostsTC, updatePostTC} = postsActions
 
-const connector = connect(mapStateToProps, {deletePostTC, fetchPostsTC});
+const connector = connect(mapStateToProps, {deletePostTC, fetchPostsTC, updatePostTC});
 
 //Connect Type
 type TProps = ConnectedProps<typeof connector>;
