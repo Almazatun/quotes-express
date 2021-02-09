02a558347aa6dfda282ca31d25aea6747d38d021
Almaz <atunstyle@gmail.com>
2021-02-09 09:10:05 +0300

    include new reducer in the Root reducer *Application reducer.

--- client/src/reducer/index.ts
+++ client/src/reducer/index.ts
@@ -2,11 +2,13 @@ import {combineReducers, configureStore} from "@reduxjs/toolkit";
 import thunkMiddleware from 'redux-thunk'
 import {postsReducer} from "../components/Posts";
 import {authReducer} from "../context";
+import {applicationReducer} from "../features/Application";
 
 
 export const rootReducer = combineReducers({
     posts: postsReducer,
-    auth: authReducer
+    auth: authReducer,
+    application: applicationReducer
 })
 
 export const store = configureStore({
@@ -18,5 +20,9 @@ export const store = configureStore({
 export type RootReducerType = typeof rootReducer
 export type AppRootStateType = ReturnType<RootReducerType>
 export type AppDispatchType = typeof store.dispatch
+export type ThunkAPIType = {
+    dispatch: (action: unknown) => unknown
+    rejectWithValue: Function
+}
 
 export type RequestStatus = 'idle' | 'loading' | 'success' | 'failed'
